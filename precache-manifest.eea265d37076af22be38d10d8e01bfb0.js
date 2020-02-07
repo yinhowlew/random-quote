b0989bc1602ab2912cf942de374c7c451fa4619d
yinhow <yinhowlew@gmail.com>
2020-02-07 18:38:05 -0500

    Updates

---
+++ precache-manifest.eea265d37076af22be38d10d8e01bfb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7e7ac8896972c5c2c2b3efed191bc8",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "83c689766b507e4b5cfa",
+    "url": "/random-quote/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "40a6444f097b943d86ef",
+    "url": "/random-quote/static/css/main.1cb8dbb2.chunk.css"
+  },
+  {
+    "revision": "83c689766b507e4b5cfa",
+    "url": "/random-quote/static/js/2.d31a1028.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/random-quote/static/js/2.d31a1028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a6444f097b943d86ef",
+    "url": "/random-quote/static/js/main.4f9dd9b8.chunk.js"
+  },
+  {
+    "revision": "f63233a257ac71fec8a8",
+    "url": "/random-quote/static/js/runtime-main.123033ce.js"
+  }
+]);
